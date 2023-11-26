6fd7577cd3f63b526b39cb4540b92cc3811a7f32
aelaraby99 <aelaraby999@gmail.com>
2023-11-27 01:21:47 +0200

    Initial Commit

---
+++ Talabat.APIs/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Talabat.APIs.Controllers
+{
+    [EnableCors("MyPolicy")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
---
+++ Talabat.Repository/Data/Migrations/20231119153814_DeleteBehavior.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Talabat.Repository.Data.Migrations
+{
+    public partial class DeleteBehavior : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_DeliveryMethods_DeliveryMethodId",
+                table: "Orders");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_DeliveryMethods_DeliveryMethodId",
+                table: "Orders",
+                column: "DeliveryMethodId",
+                principalTable: "DeliveryMethods",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_DeliveryMethods_DeliveryMethodId",
+                table: "Orders");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_DeliveryMethods_DeliveryMethodId",
+                table: "Orders",
+                column: "DeliveryMethodId",
+                principalTable: "DeliveryMethods",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
